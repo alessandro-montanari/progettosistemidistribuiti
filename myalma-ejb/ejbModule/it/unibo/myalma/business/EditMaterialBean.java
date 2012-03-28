af5a67049c355d568c2c474126214bf987689c3c
alemonty88@gmail.com <alemonty88@gmail.com@3133ea3a-402a-92a0-f7b1-85a96549d6d5>
2012-03-28 13:47:00 +0000

    Quasi stabile

---
+++ myalma-ejb/ejbModule/it/unibo/myalma/business/EditMaterialBean.java
@@ -0,0 +1,94 @@
+package it.unibo.myalma.business;
+
+import java.io.File;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import it.unibo.myalma.model.Material;
+
+@Stateful
+@Name("materialManager")
+@Scope(ScopeType.CONVERSATION)
+@Remote(IEditMaterial.class)
+@RolesAllowed({"professor", "admin"})
+public class EditMaterialBean extends EditContentBean implements IEditMaterial 
+{
+	private byte[] fileData = null;
+	private String fileName = "";
+	private FileManager fileManager = null;
+
+	public EditMaterialBean()
+	{
+		fileManager = new FileManager();
+	}
+
+	@Override
+	public void setFileData(byte[] data) 
+	{
+		fileData = data;
+	}
+
+	@Override
+	public byte[] getFileData() 
+	{
+		return fileData;
+	}
+
+	@Override
+	public void setFileInfo(String fileName, byte[] data) 
+	{
+		setFileName(fileName);
+		setFileData(data);
+	}
+
+	@Override
+	public void setFileName(String name) 
+	{
+		fileName = name;
+	}
+
+	@Override
+	public String getFileName() 
+	{
+		return fileName;
+	}
+
+	@Override
+	public void save() 
+	{
+		Material material = (Material)this.getContent();
+		
+		//Controllo se c'è già un file collegato al conteuto
+		File file = new File(material.getPath());
+		if(file.exists())
+			fileManager.deleteFile(material.getPath());
+		
+		// Devo salvare il file prima di salvare il contenuto nel DB perché mi serve il path
+		String path = fileManager.saveFile(fileName, fileData);
+		material.setPath(path);
+		
+		try
+		{
+			super.save();
+		}
+		catch(Exception e)
+		{
+			// Se il salvataggio non va a buon fine elimino il file appena inserito per non lasciare file volanti
+			fileManager.deleteFile(path);
+		}
+	}
+
+	@Override
+	public void delete() 
+	{
+		Material material = (Material)this.getContent();
+		fileManager.deleteFile(material.getPath());
+		super.delete();
+	}
+}
