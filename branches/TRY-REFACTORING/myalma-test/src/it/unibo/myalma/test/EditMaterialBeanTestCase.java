0bb5bd478ec61438cff53e13d8accc634b638247
alemonty88@gmail.com <alemonty88@gmail.com@3133ea3a-402a-92a0-f7b1-85a96549d6d5>
2012-05-31 08:00:05 +0000

    Refactoring EditMaterialBean + Test

--- branches/TRY-REFACTORING/myalma-test/src/it/unibo/myalma/test/EditMaterialBeanTestCase.java
+++ branches/TRY-REFACTORING/myalma-test/src/it/unibo/myalma/test/EditMaterialBeanTestCase.java
@@ -7,9 +7,9 @@ import java.io.File;
 import it.unibo.myalma.business.professor.IEditMaterial;
 import it.unibo.myalma.business.search.ISearch;
 import it.unibo.myalma.model.ContentType;
+import it.unibo.myalma.model.Material;
 import it.unibo.myalma.test.helpers.AutomateImportMYSQL;
 import it.unibo.myalma.test.helpers.TestHelper;
-import it.unibo.myalma.business.util.FileManager;
 
 import org.junit.After;
 import org.junit.Before;
@@ -24,7 +24,8 @@ public class EditMaterialBeanTestCase {
 	static IEditMaterial editMaterialBean = null;
 	// DA MODIFICARE
 	static String serverPath = "/Users/ale/Documents/Magistrale/Anno2/SistemiDistribuiti/Strumenti/jboss-6.1.0.Final/server/default/data/myalma/";
-
+	static byte[] data = new byte[] {12, 34, 67};
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception 
 	{
@@ -53,47 +54,65 @@ public class EditMaterialBeanTestCase {
 		int previousMaterials = searchBean.findContentsByType(ContentType.MATERIAL).size();
 		String fileName = "test-file.dat";
 		editMaterialBean.newContent(ContentType.MATERIAL);
-		editMaterialBean.setFileInfo(fileName, new byte[]{34, 54, 65});
+		editMaterialBean.setFileInfo(fileName, data);
 		editMaterialBean.setParentContentId(searchBean.findContentsByType(ContentType.CATEGORY).get(0).getId());
 		editMaterialBean.save();
 		assertEquals(previousMaterials+1, searchBean.findContentsByType(ContentType.MATERIAL).size());
 		File file = new File(serverPath + fileName);
 		assertTrue(file.exists());
 	}
-
+	
 	@Test
-	public void testDelete() {
-		fail("Not yet implemented");
+	public void testSaveWithError() 
+	{
+		int previousMaterials = searchBean.findContentsByType(ContentType.MATERIAL).size();
+		String fileName = "test-file.dat";
+		editMaterialBean.newContent(ContentType.MATERIAL);
+		editMaterialBean.setFileInfo(fileName, data);
+		editMaterialBean.save();	// Scatterà un eccezione perché non è possibile appendere un contenuto ad una categoria null
+		
+		// Controllo che il materiale non sia inserito e che il file non sia salvato
+		assertEquals(previousMaterials, searchBean.findContentsByType(ContentType.MATERIAL).size());
+		File file = new File(serverPath + fileName);
+		assertFalse(file.exists());
 	}
 
 	@Test
-	public void testEditMaterialBean() {
-		fail("Not yet implemented");
+	public void testDelete() 
+	{
+		int previousMaterials = searchBean.findContentsByType(ContentType.MATERIAL).size();
+		Material material = (Material)searchBean.findContentsByTitle("Materiale").get(0);
+		editMaterialBean.setContentId(material.getId());
+		editMaterialBean.edit();
+		editMaterialBean.delete();
+		assertEquals(previousMaterials-1, searchBean.findContentsByType(ContentType.MATERIAL).size());
+		File file = new File(material.getPath());
+		assertFalse(file.exists());
 	}
-
+	
 	@Test
-	public void testSetFileData() {
-		fail("Not yet implemented");
+	public void testFileData()
+	{
+		editMaterialBean.setFileData(data);
+		assertEquals(data[0], editMaterialBean.getFileData()[0]);
+		assertEquals(data[1], editMaterialBean.getFileData()[1]);
+		assertEquals(data[2], editMaterialBean.getFileData()[2]);
 	}
-
+	
 	@Test
-	public void testGetFileData() {
-		fail("Not yet implemented");
+	public void testFileInfo()
+	{
+		editMaterialBean.setFileInfo("test", data);
+		assertEquals("test", editMaterialBean.getFileName());
+		assertEquals(data[0], editMaterialBean.getFileData()[0]);
+		assertEquals(data[1], editMaterialBean.getFileData()[1]);
+		assertEquals(data[2], editMaterialBean.getFileData()[2]);
 	}
-
+	
 	@Test
-	public void testSetFileInfo() {
-		fail("Not yet implemented");
+	public void testFileName()
+	{
+		editMaterialBean.setFileInfo("test", data);
+		assertEquals("test", editMaterialBean.getFileName());
 	}
-
-	@Test
-	public void testSetFileName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFileName() {
-		fail("Not yet implemented");
-	}
-
 }
